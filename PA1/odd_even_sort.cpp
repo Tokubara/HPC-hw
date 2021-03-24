84136a848944cfd42b5cc8c1427d2736fdc90454
Tokubara <1270802817@qq.com>
2021-03-24 16:48:39 +0800

    (init) PA1

---
+++ PA1/odd_even_sort.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <mpi.h>
+
+#include "worker.h"
+
+void Worker::sort() {
+  /** Your code ... */
+  // you can use variables in class Worker: n, nprocs, rank, block_len, data
+}

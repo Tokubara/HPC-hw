b264cc04458740f43c11aac13cc0c29a9d6889f1
Tokubara <1270802817@qq.com>
2021-03-27 10:48:13 +0800

    说明变量可以套变量,并且修复了for多了一次为空的情况

--- PA1/submit.sh
+++ PA1/submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -e
+set -e
 
 # source /home/spack/spack/share/spack/setup-env.sh
 
@@ -7,14 +7,15 @@
 
 # make -j 4
 
-# job_name=./odd_even_sort
+job_name=./odd_even_sort
 
-# data_file=data/$1.dat
+num=100000000
+data_file=data/$num.dat
 
 declare -a srun_config
 srun_config=(1 1 1 2 1 4 1 16 2 32)
-for((i=0;i<=${#srun_config[@]};i+=2)) do
-	echo ${srun_config[$i]} ${srun_config[$(($i+1))]}
+for((i=0;i<${#srun_config[@]};i+=2)) do
+	echo srun -N ${srun_config[$i]} -n ${srun_config[$(($i+1))]} --cpu-bind sockets $job_name $num $data_file
 done
 
 # run mpi_pow
@@ -24,5 +25,5 @@ done
 # srun -N 1 -n 16  --cpu-bind sockets $job_name $1 $data_file
 # srun -N 2 -n 32  --cpu-bind sockets $job_name $1 $data_file
 
-# echo All done!
+echo All done!
 

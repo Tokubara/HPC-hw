6bfe69e2ba4e6ac4e9be39bd8d23dd9078fe1a42
Tokubara <1270802817@qq.com>
2021-03-27 10:33:22 +0800

    不行, must use subscript when assigning associative array

--- PA1/submit.sh
+++ PA1/submit.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-set -e
+# set -e
 
-source /home/spack/spack/share/spack/setup-env.sh
+# source /home/spack/spack/share/spack/setup-env.sh
 
-spack load openmpi
+# spack load openmpi
 
-make -j 4
+# make -j 4
 
-job_name=./odd_even_sort
+# job_name=./odd_even_sort
 
-data_file=data/$1.dat
+# data_file=data/$1.dat
+
+declare -A srun_config
+srun_config=(1 1 1 2 1 4 1 16 2 32)
+for((i=1;i<=${#srun_config};i+=2)) do
+	echo ${srun_config[i]} ${srun_config[i+1]}
+done
 
 # run mpi_pow
-srun -N 1 -n 1   --cpu-bind sockets $job_name $1 $data_file
-srun -N 1 -n 2   --cpu-bind sockets $job_name $1 $data_file
-srun -N 1 -n 4  --cpu-bind sockets $job_name $1 $data_file
-srun -N 1 -n 16  --cpu-bind sockets $job_name $1 $data_file
-srun -N 2 -n 32  --cpu-bind sockets $job_name $1 $data_file
+# srun -N 1 -n 1   --cpu-bind sockets $job_name $1 $data_file
+# srun -N 1 -n 2   --cpu-bind sockets $job_name $1 $data_file
+# srun -N 1 -n 4  --cpu-bind sockets $job_name $1 $data_file
+# srun -N 1 -n 16  --cpu-bind sockets $job_name $1 $data_file
+# srun -N 2 -n 32  --cpu-bind sockets $job_name $1 $data_file
 
-echo All done!
+# echo All done!
 

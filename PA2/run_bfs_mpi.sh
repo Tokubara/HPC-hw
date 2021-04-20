031a7bb5528c751a4635931804bd37fe67683562
Tokubara <1270802817@qq.com>
2021-04-20 16:32:56 +0800

    修改Makefile等关于run的逻辑, runo和runm目标, 两个run的bash脚本是500m图, 进程, 线程开到最大

--- PA2/run_bfs_mpi.sh
+++ PA2/run_bfs_mpi.sh
@@ -6,5 +6,5 @@ export OMP_NUM_THREADS=28
 export OMP_PROC_BIND=true
 export OMP_PLACES=cores
 
-srun -N 4 -n 4 ./bfs_omp_mpi /home/course/hpc/assignments/2021/PA2/graph/68m.graph 
+srun -N 4 -n 4 ./bfs_omp_mpi /home/course/hpc/assignments/2021/PA2/graph/500m.graph 
 
--- PA2/run_bfs_omp.sh
+++ PA2/run_bfs_omp.sh
@@ -5,6 +5,6 @@
 export OMP_NUM_THREADS=28
 export OMP_PLACES=cores
 
-srun -n 1 ./bfs_omp $ass/graph/68m.graph
+srun -n 1 ./bfs_omp $ass/graph/500m.graph
 echo done
 

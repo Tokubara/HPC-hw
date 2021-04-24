d16a5d16fd27a60745aa35d934a2b4d7b4f4586b
Tokubara <1270802817@qq.com>
2021-04-24 23:03:30 +0800

    修改submit脚本, 但这里提交是因为, 非常奇怪, 我以为openmp所做的改变是等价的, 但似乎完全不是这样, 运行要4000ms之久

--- PA2/run_bfs_omp.sh
+++ PA2/run_bfs_omp.sh
@@ -5,7 +5,7 @@
 export OMP_NUM_THREADS=28
 export OMP_PLACES=cores
 
-# srun -n 1 ./bfs_omp $ass/graph/500m.graph
-./bfs_omp ../graph/500k.graph
+srun -n 1 ./bfs_omp $ass/graph/500m.graph
+# ./bfs_omp ../graph/500k.graph
 echo done
 
--- PA2/submit_omp.sh
+++ PA2/submit_omp.sh
@@ -3,17 +3,20 @@ if ! [[ -d log ]]; then
 	mkdir log
 fi
 
-result_file=log/result_opm_log.txt
+result_file=log/result_omp_log.txt
 echo -n > $result_file # 清空文件内容
 
 # {{{1 运行参数控制
 config=( 1 7 14 28 )
+export OMP_PROC_BIND=true
 export OMP_PLACES=cores
 ass=/home/course/hpc/assignments/2021/PA2/
+# ass=../
 for i in ${config[@]};do
   export OMP_NUM_THREADS=$i
   # echo threads $OMP_NUM_THREADS
   # echo srun -n 1 ./bfs_omp $ass/graph/68m.graph
-  srun -n 1 ./bfs_omp $ass/graph/68m.graph >> $result_file
+  # ./bfs_omp $ass/graph/500k.graph >> $result_file
+  srun -n 1 ./bfs_omp $ass/graph/${1}m.graph >> $result_file
 done
 echo done

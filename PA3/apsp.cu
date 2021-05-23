d9e74c25c1a60392f30e8a3a2f7532d4d5ea5c87
Tokubara <1270802817@qq.com>
2021-05-23 16:17:26 +0800

    修改变量名和benchmark改回原样

--- PA3/apsp.cu
+++ PA3/apsp.cu
@@ -18,153 +18,141 @@ namespace { // 为什么要namespace?
 
 }
 
-__global__ void blocked_fw_phase1(const int blockId, int n, int* const graph) {
-    __shared__ int cacheGraph[BLOCK_SIZE][BLOCK_SIZE]; // 对角块的shared存储, 即使是右下角块, 空间也是开够了的
+__global__ void blocked_fw_phase1(const int block_id, int n, int* const graph) {
+    __shared__ int cache_diag[BLOCK_SIZE][BLOCK_SIZE]; // 对角块的shared存储, 即使是右下角块, 空间也是开够了的
     const int idx = threadIdx.x; // 块内的列号 // TODO 我担心这样是不是太浪费寄存器了
     const int idy = threadIdx.y;
 
-    const int v1 = BLOCK_SIZE * blockId + idy; // 整个graph矩阵的行号
-    const int v2 = BLOCK_SIZE * blockId + idx;
+    const int global_row_id = BLOCK_SIZE * block_id + idy; // 整个graph矩阵的行号
+    const int global_col_id = BLOCK_SIZE * block_id + idx;
 
-    int newPath;
+    int new_len;
 
-    const int cellId = v1 * n + v2; // 在graph中对应的对角块的索引
-    cacheGraph[idy][idx] = (v1 < n && v2 < n)?graph[cellId]:MAX_DISTANCE; // 处理右下角的对角块太小的问题, 避免越界
+    const int global_id = global_row_id * n + global_col_id; // 在graph中对应的对角块的索引
+    cache_diag[idy][idx] = (global_row_id < n && global_col_id < n)?graph[global_id]:MAX_DISTANCE; // 处理右下角的对角块太小的问题, 避免越界
     #pragma unroll
     for (int u = 0; u < BLOCK_SIZE; ++u) { // FW的外层循环
         __syncthreads(); // 下一次循环前得同步
-        newPath = cacheGraph[idy][u] + cacheGraph[u][idx]; // newPath省掉能不能节省一点寄存器?
+        new_len = cache_diag[idy][u] + cache_diag[u][idx]; // new_len省掉能不能节省一点寄存器?
 
-        if (newPath < cacheGraph[idy][idx]) {
-            cacheGraph[idy][idx] = newPath;
+        if (new_len < cache_diag[idy][idx]) {
+            cache_diag[idy][idx] = new_len;
         }
     }
 
-    if (v1 < n && v2 < n) {
-        graph[cellId] = cacheGraph[idy][idx]; // 写回graph
+    if (global_row_id < n && global_col_id < n) {
+        graph[global_id] = cache_diag[idy][idx]; // 写回graph
     }
 }
 
-__global__ void blocked_fw_phase2(const int blockId, const int n, int* const graph) {
-    if (blockIdx.x == blockId) return; // x的范围是0到2G-1, 交叉就是对角块, 是blockId
+__global__ void blocked_fw_phase2(const int block_id, const int n, int* const graph) {
+    if (blockIdx.x == block_id) return; // x的范围是0到2G-1, 交叉就是对角块, 是block_id
 
     const int idx = threadIdx.x;
     const int idy = threadIdx.y;
 
-    int v1 = BLOCK_SIZE * blockId + idy; // v1存的是对角块对应的位置
-    int v2 = BLOCK_SIZE * blockId + idx;
+    int global_row_id = BLOCK_SIZE * block_id + idy; // global_row_id存的是对角块对应的位置
+    int global_col_id = BLOCK_SIZE * block_id + idx;
 
-    __shared__ int cacheGraphBase[BLOCK_SIZE][BLOCK_SIZE];
-
+    __shared__ int cache_diag[BLOCK_SIZE][BLOCK_SIZE];
     // Load base block for graph and predecessors
-    int cellId = v1 * n + v2;
-
-    cacheGraphBase[idy][idx] = (v1 < n && v2 < n)?graph[cellId]:MAX_DISTANCE;
+    int global_id = global_row_id * n + global_col_id;
+    cache_diag[idy][idx] = (global_row_id < n && global_col_id < n)?graph[global_id]:MAX_DISTANCE;
 
     // blockIdx.y==0对应的是行块, blockIdx.y==1对应的是列块
     if (blockIdx.y == 0) {
-        v2 = BLOCK_SIZE * blockIdx.x + idx; // 行块需要修改列号, 列块需要修改行号
+        global_col_id = BLOCK_SIZE * blockIdx.x + idx; // 行块需要修改列号, 列块需要修改行号
     } else {
-        v1 = BLOCK_SIZE * blockIdx.x + idy;
+        global_row_id = BLOCK_SIZE * blockIdx.x + idy;
     }
+    global_id = global_row_id * n + global_col_id;
 
-    __shared__ int cacheGraph[BLOCK_SIZE][BLOCK_SIZE]; // 把负责的graph块拷贝到了cacheGraph
-
-    int currentPath;
-
-    cellId = v1 * n + v2;
-    currentPath = (v1 < n && v2 < n)?graph[cellId]:MAX_DISTANCE;
-
-    cacheGraph[idy][idx] = currentPath; 
+    __shared__ int cache_self[BLOCK_SIZE][BLOCK_SIZE]; // 把负责的graph块拷贝到了cache_self
+    int cur_len = (global_row_id < n && global_col_id < n)?graph[global_id]:MAX_DISTANCE;
+    cache_self[idy][idx] = cur_len;  // 载入共享内存
 
     __syncthreads();
 
-    int newPath;
+    int new_len;
 
     if (blockIdx.y == 0) { // 处理行块
         #pragma unroll
         for (int u = 0; u < BLOCK_SIZE; ++u) {
-            newPath = cacheGraphBase[idy][u] + cacheGraph[u][idx];
+            new_len = cache_diag[idy][u] + cache_self[u][idx];
 
-            if (newPath < currentPath) {
-                currentPath = newPath;
+            if (new_len < cur_len) {
+                cur_len = new_len;
             }
 
-            cacheGraph[idy][idx] = currentPath;
+            cache_self[idy][idx] = cur_len;
             __syncthreads();
         }
     } else {
         #pragma unroll
         for (int u = 0; u < BLOCK_SIZE; ++u) {
-            newPath = cacheGraph[idy][u] + cacheGraphBase[u][idx];
+            new_len = cache_self[idy][u] + cache_diag[u][idx];
 
-            if (newPath < currentPath) {
-                currentPath = newPath;
+            if (new_len < cur_len) {
+                cur_len = new_len;
             }
 
-            cacheGraph[idy][idx] = currentPath;
+            cache_self[idy][idx] = cur_len;
             __syncthreads();
         }
     }
 
-    if (v1 < n && v2 < n) {
-        graph[cellId] = currentPath;
+    if (global_row_id < n && global_col_id < n) {
+        graph[global_id] = cur_len;
     }
 }
 
-__global__ void blocked_fw_phase3(const int blockId, const int n, int* const graph) {
-    if (blockIdx.x == blockId || blockIdx.y == blockId) return; // 前两个阶段的块
+__global__ void blocked_fw_phase3(const int block_id, const int n, int* const graph) {
+    if (blockIdx.x == block_id || blockIdx.y == block_id) return; // 前两个阶段的块
 
     const int idx = threadIdx.x;
     const int idy = threadIdx.y;
 
-    const int v1 = blockDim.y * blockIdx.y + idy; // 在graph矩阵中负责的位置
-    const int v2 = blockDim.x * blockIdx.x + idx;
+    const int global_row_id = blockDim.y * blockIdx.y + idy; // 在graph矩阵中负责的位置
+    const int global_col_id = blockDim.x * blockIdx.x + idx;
+    const int cache_row_id = BLOCK_SIZE * block_id + idy;
+    const int cache_col_id = BLOCK_SIZE * block_id + idx;
 
-    __shared__ int cacheGraphBaseRow[BLOCK_SIZE][BLOCK_SIZE]; // 对应的行块
-    __shared__ int cacheGraphBaseCol[BLOCK_SIZE][BLOCK_SIZE]; // 对应的列块
+    __shared__ int cache_row[BLOCK_SIZE][BLOCK_SIZE]; // 对应的行块
+    __shared__ int cache_col[BLOCK_SIZE][BLOCK_SIZE]; // 对应的列块
 
-    int v1Row = BLOCK_SIZE * blockId + idy; // 此元素对应的行块元素的行号
-    int v2Col = BLOCK_SIZE * blockId + idx;
+    cache_row[idy][idx] = (cache_row_id < n && global_col_id < n)?graph[cache_row_id * n + global_col_id]:MAX_DISTANCE; // 写对应的行块
+    cache_col[idy][idx] = (global_row_id  < n && cache_col_id < n)?graph[global_row_id * n + cache_col_id]:MAX_DISTANCE; // 写对应的列块
 
-
-    cacheGraphBaseRow[idy][idx] = (v1Row < n && v2 < n)?graph[v1Row * n + v2]:MAX_DISTANCE;
-    cacheGraphBaseCol[idy][idx] = (v1  < n && v2Col < n)?graph[v1 * n + v2Col]:MAX_DISTANCE;
-
-    // Synchronize to make sure the all value are loaded in virtual block
    __syncthreads();
+   
+   if (global_row_id  < n && global_col_id < n) {
+       int global_id = global_row_id * n + global_col_id;
+       int cur_len = graph[global_id];
 
-   int cellId;
-   int currentPath;
-   int newPath;
-
-   // 不需要同步, 因为用不上
-   if (v1  < n && v2 < n) {
-       cellId = v1 * n + v2;
-       currentPath = graph[cellId];
+       int new_len;
 
         #pragma unroll
        for (int u = 0; u < BLOCK_SIZE; ++u) {
-           newPath = cacheGraphBaseCol[idy][u] + cacheGraphBaseRow[u][idx];
-           if (currentPath > newPath) {
-               currentPath = newPath;
-           }
+           new_len = cache_col[idy][u] + cache_row[u][idx];
+           if (cur_len > new_len) {
+               cur_len = new_len;
+           } // 不需要同步, 因为用不上
        }
-       graph[cellId] = currentPath;
+       graph[global_id] = cur_len;
     } 
 }
 
 void apsp(int n, /* device */ int *graph) { // graph是device内存上的
-    int numBlock = (n - 1) / BLOCK_SIZE + 1;
-    dim3 gridPhase1(1 ,1, 1);
-    dim3 gridPhase2(numBlock, 2 , 1);
-    dim3 gridPhase3(numBlock, numBlock, 1);
-    dim3 dimBlockSize(BLOCK_SIZE, BLOCK_SIZE, 1);
+    int num_block = (n - 1) / BLOCK_SIZE + 1;
+    dim3 grid_phase1(1 ,1, 1);
+    dim3 grid_phase2(num_block, 2 , 1);
+    dim3 grid_phase3(num_block, num_block, 1);
+    dim3 block_size(BLOCK_SIZE, BLOCK_SIZE, 1);
 
-    for (int blockID = 0; blockID < numBlock; ++blockID) {
-         blocked_fw_phase1<<<gridPhase1, dimBlockSize>>>(blockID, n, graph);
-         blocked_fw_phase2<<<gridPhase2, dimBlockSize>>>(blockID, n, graph);
-         blocked_fw_phase3<<<gridPhase3, dimBlockSize>>>(blockID, n, graph);
+    for (int blockID = 0; blockID < num_block; ++blockID) {
+         blocked_fw_phase1<<<grid_phase1, block_size>>>(blockID, n, graph);
+         blocked_fw_phase2<<<grid_phase2, block_size>>>(blockID, n, graph);
+         blocked_fw_phase3<<<grid_phase3, block_size>>>(blockID, n, graph);
     }
 }
 
